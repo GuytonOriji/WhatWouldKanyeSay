d5bca837b4e7c400cb1d9bcad9b20798072bf482
guytonoriji <guyton.m.oriji.578@gmail.com>
2020-02-20 09:25:53 -0500

    Updates

---
+++ precache-manifest.e2d0af0f39413f481cc1c4aeec01c834.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a08e86f34754e9e7f4d7908efffea4e",
+    "url": "/WhatWouldKanyeSay/index.html"
+  },
+  {
+    "revision": "c2b9a9418165d62320b0",
+    "url": "/WhatWouldKanyeSay/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "aa43793241fdd630907d",
+    "url": "/WhatWouldKanyeSay/static/css/main.b8a31076.chunk.css"
+  },
+  {
+    "revision": "c2b9a9418165d62320b0",
+    "url": "/WhatWouldKanyeSay/static/js/2.1d020f10.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/WhatWouldKanyeSay/static/js/2.1d020f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa43793241fdd630907d",
+    "url": "/WhatWouldKanyeSay/static/js/main.a205aa29.chunk.js"
+  },
+  {
+    "revision": "51eff1b67b3c925df0e1",
+    "url": "/WhatWouldKanyeSay/static/js/runtime-main.5534f5cf.js"
+  },
+  {
+    "revision": "89e036668365bb2d46b2699ade5af3bc",
+    "url": "/WhatWouldKanyeSay/static/media/kayneThinkingFace.89e03666.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WhatWouldKanyeSay/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f09436cbad0d36b17a95540409293646",
+    "url": "/WhatWouldKanyeSay/static/media/westkanye.f09436cb.jpg"
+  }
+]);
